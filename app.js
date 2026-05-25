bf66cb91362d4926d0e7a0bc7fe78dd940dc0840
Nefi Avila <avila.nefi1280@gmail.com>
2026-05-25 17:16:27 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const tabla = document.getElementById("tabla");
+const buscar = document.getElementById("buscar");
+
+async function cargarDatos(texto = "") {
+
+  let query = client
+    .from("camisetas")
+    .select("*");
+
+  if(texto){
+
+    query = query.ilike(
+      "estudiante",
+      `%${texto}%`
+    );
+  }
+
+  const { data, error } = await query;
+
+  if(error){
+    console.log(error);
+    return;
+  }
+
+  tabla.innerHTML = "";
+
+  data.forEach(item => {
+
+    tabla.innerHTML += `
+      <tr>
+        <td>${item.genero}</td>
+        <td>${item.estudiante}</td>
+        <td>${item.talla}</td>
+        <td>${item.numero}</td>
+        <td>${item.observacion}</td>
+      </tr>
+    `;
+  });
+}
+
+buscar.addEventListener("keyup", (e)=>{
+
+  cargarDatos(e.target.value);
+
+});
+
+cargarDatos();
---
+++ config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://kdblwgnvmsovfhmwyiey.supabase.co";
+const SUPABASE_KEY = "sb_publishable_cqyLhMOoUUmzEaVjSB3Jog_hJoVsube";
+
+const client = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
